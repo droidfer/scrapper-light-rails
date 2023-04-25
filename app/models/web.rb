043da3089613de6100a62b1e32335e88b51cc044
Fernando <fmartinez@dotech.com.py>
2023-04-25 10:10:47 -0400

    Set message on processing for the search

--- app/models/web.rb
+++ app/models/web.rb
@@ -4,7 +4,13 @@ class Web < ApplicationRecord
   validates :url, presence: true
   
   def display_name
+    return "#{url}" unless finished_at
     return "No Title: #{url}" unless name.present? 
     name
+  end
+  
+  def display_total
+    return "in progress" unless finished_at
+    total
   end
 end
