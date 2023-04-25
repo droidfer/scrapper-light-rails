35d77eedcbc31163e114599a1c9bd7327af4593f
Fernando <fmartinez@dotech.com.py>
2023-04-25 14:57:29 -0400

    Adding test to web and weblink models

--- spec/models/web_spec.rb
+++ spec/models/web_spec.rb
@@ -7,8 +7,43 @@ RSpec.describe Web, type: :model do
   end
 
   it 'Invalid web with nil url' do
-    company_min = FactoryBot.build(:web, url: nil)
-    expect(company_min).to_not be_valid
+    web_nil = FactoryBot.build(:web, url: nil)
+    expect(web_nil).to_not be_valid
+  end
+
+  it 'Invalid web with wrong url format' do
+    web_nil = FactoryBot.build(:web, url: 'www.google.com') #a valid URL will be including http:// to that string
+    expect(web_nil).to_not be_valid
+  end
+
+  it 'display_name on process is the url' do
+    web = FactoryBot.build(:web)
+    expect(web.display_name).to eq(web.url)
+  end
+
+  it 'display_name on finished_at is the name' do
+    web = FactoryBot.build(:web, finished_at: DateTime.now.to_date)
+    expect(web.display_name).to eq(web.name)
+  end
+
+  it 'display_name on finished_at with nil name is No Title: URL' do
+    web = FactoryBot.build(:web, finished_at: DateTime.now.to_date, name: nil)
+    expect(web.display_name).to eq("No Title: #{web.url}")
+  end
+
+  it 'display_total on process is in progress' do
+    web = FactoryBot.build(:web)
+    expect(web.display_total).to eq("in progress")
+  end
+
+  it 'display_total status fail is error' do
+    web = FactoryBot.build(:web, status: 2, finished_at: DateTime.now.to_date)
+    expect(web.display_total).to eq("error")
+  end
+
+  it 'display_total status success is total' do
+    web = FactoryBot.build(:web, status: 1, finished_at: DateTime.now.to_date, total: 5)
+    expect(web.display_total).to eq(web.total)
   end
 
 end
--- spec/models/weblink_spec.rb
+++ spec/models/weblink_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Weblink, type: :model do
   it 'valid weblink' do
     expect(FactoryBot.build(:weblink)).to be_valid
   end
+
+  it 'display_name on nil name is No Name: URL' do
+    weblink = FactoryBot.build(:weblink, name: nil)
+    expect(weblink.display_name).to eq("No Name: #{weblink.url}")
+  end
+
+  it 'display_name on nil name is nameL' do
+    weblink = FactoryBot.build(:weblink)
+    expect(weblink.display_name).to eq("#{weblink.name}")
+  end
+  
 end
